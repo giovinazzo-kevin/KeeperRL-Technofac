0b09a7a8d2e48898fec4b94fe88de48313f99ee5
Kevin Giovinazzo <kevin.giovinazzo@horsa.it>
2021-11-03 00:37:38 +0100

    fix build script

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 if test $# -ne 1; then 
     echo 'Usage: build.sh <mod folder>'
 else
+    ls -ld $(find .) | grep -oh \./$1/[^_].*\.txt | xargs -I{} sh -c "cp {} ."
     ls -ld $(find .) | grep -oh \./stdlib/_.*\.txt | xargs -I{} sh -c "cat {}; echo ''" > furniture.txt
     ls -ld $(find .) | grep -oh \./$1/_.*\.txt | xargs -I{} sh -c "cat {}; echo ''" >> furniture.txt
-    ls -ld $(find .) | grep -oh \./$1/[^_].*\.txt | xargs -I{} sh -c "cp {} ."
 fi
