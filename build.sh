c1e7d781cbbf0325a20e6f90dc31b140be8d2ffb
Kevin Giovinazzo <kevin.giovinazzo@horsa.it>
2021-11-06 06:26:06 +0100

    add multitile example

--- build.sh
+++ build.sh
@@ -5,5 +5,7 @@ else
     rm ./*.txt
     ls -ld $(find .) | grep -oh \./$1/[^_].*\.txt | xargs -I{} sh -c "cp {} ."
     ls -ld $(find .) | grep -oh \./stdlib/_.*\.txt | xargs -I{} sh -c "cat {}; echo ''" > furniture.txt
+    ls -ld $(find .) | grep -oh \./stdlib/_.*\.txt | xargs -I{} sh -c "cat {}; echo ''" > spells.txt
     ls -ld $(find .) | grep -oh \./$1/_.*\.txt | xargs -I{} sh -c "cat {}; echo ''" >> furniture.txt
+    ls -ld $(find .) | grep -oh \./$1/spells\.txt | xargs -I{} sh -c "cat {}; echo ''" >> spells.txt
 fi
