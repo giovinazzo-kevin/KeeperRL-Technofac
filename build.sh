170261d7b0cd19f5e6269159fee57208fe72ec18
Kevin Giovinazzo <kevin.giovinazzo@horsa.it>
2021-11-07 10:04:30 +0100

    all nighter succesfully pulled

--- build.sh
+++ build.sh
@@ -7,5 +7,7 @@ else
     ls -ld $(find .) | grep -oh \./stdlib/_.*\.txt | xargs -I{} sh -c "cat {}; echo ''" > furniture.txt
     ls -ld $(find .) | grep -oh \./stdlib/_.*\.txt | xargs -I{} sh -c "cat {}; echo ''" > spells.txt
     ls -ld $(find .) | grep -oh \./$1/_.*\.txt | xargs -I{} sh -c "cat {}; echo ''" >> furniture.txt
+    ls -ld $(find .) | grep -oh \./$1/_.*\.txt | xargs -I{} sh -c "cat {}; echo ''" >> spells.txt
+    ls -ld $(find .) | grep -oh \./$1/furniture\.txt | xargs -I{} sh -c "cat {}; echo ''" >> furniture.txt
     ls -ld $(find .) | grep -oh \./$1/spells\.txt | xargs -I{} sh -c "cat {}; echo ''" >> spells.txt
 fi
