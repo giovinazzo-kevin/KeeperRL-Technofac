6ee552d703ab6d87ad120ff204fc59bbc4b85bfc
Kevin Giovinazzo <kevin.giovinazzo@horsa.it>
2021-11-11 16:35:39 +0100

    Reimplement stdlib, WIP waves mode

--- build.sh
+++ build.sh
@@ -6,8 +6,21 @@ else
     ls -ld $(find .) | grep -oh \./$1/[^_].*\.txt | xargs -I{} sh -c "cp {} ."
     ls -ld $(find .) | grep -oh \./stdlib/_.*\.txt | xargs -I{} sh -c "cat {}; echo ''" > furniture.txt
     ls -ld $(find .) | grep -oh \./stdlib/_.*\.txt | xargs -I{} sh -c "cat {}; echo ''" > spells.txt
+    ls -ld $(find .) | grep -oh \./stdlib/_.*\.txt | xargs -I{} sh -c "cat {}; echo ''" > creatures.txt
+    ls -ld $(find .) | grep -oh \./stdlib/_.*\.txt | xargs -I{} sh -c "cat {}; echo ''" > items.txt
+
+    ls -ld $(find .) | grep -oh \./stdlib/furniture\.txt | xargs -I{} sh -c "cat {}; echo ''" >> furniture.txt
+    ls -ld $(find .) | grep -oh \./stdlib/creatures\.txt | xargs -I{} sh -c "cat {}; echo ''" >> creatures.txt
+    ls -ld $(find .) | grep -oh \./stdlib/spells\.txt | xargs -I{} sh -c "cat {}; echo ''" >> spells.txt
+    ls -ld $(find .) | grep -oh \./stdlib/items\.txt | xargs -I{} sh -c "cat {}; echo ''" >> items.txt
+
     ls -ld $(find .) | grep -oh \./$1/_.*\.txt | xargs -I{} sh -c "cat {}; echo ''" >> furniture.txt
     ls -ld $(find .) | grep -oh \./$1/_.*\.txt | xargs -I{} sh -c "cat {}; echo ''" >> spells.txt
+    ls -ld $(find .) | grep -oh \./$1/_.*\.txt | xargs -I{} sh -c "cat {}; echo ''" >> creatures.txt
+    ls -ld $(find .) | grep -oh \./$1/_.*\.txt | xargs -I{} sh -c "cat {}; echo ''" >> items.txt
+
     ls -ld $(find .) | grep -oh \./$1/furniture\.txt | xargs -I{} sh -c "cat {}; echo ''" >> furniture.txt
     ls -ld $(find .) | grep -oh \./$1/spells\.txt | xargs -I{} sh -c "cat {}; echo ''" >> spells.txt
+    ls -ld $(find .) | grep -oh \./$1/creatures\.txt | xargs -I{} sh -c "cat {}; echo ''" >> creatures.txt
+    ls -ld $(find .) | grep -oh \./$1/items\.txt | xargs -I{} sh -c "cat {}; echo ''" >> items.txt
 fi
